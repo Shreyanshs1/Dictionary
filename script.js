472a543c56c7b35e5fd95eb70bf902c240d6b9ff
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-09-18 12:37:37 +0530

    Improve UI

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const result = document.querySelector('.container');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(e.target.children[0].value);
   getWordinfo(e.target.children[0].value);
 })
 
@@ -12,15 +11,14 @@ const getWordinfo = async (word) => {
   const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
   const data = await response.json();
   const definitions = data[0].meanings[0].definitions[0];
-    console.log(definitions.synonyms)
 
   result.innerHTML = `
   <h2><strong>${data[0].word.charAt(0).toUpperCase()+ data[0].word.slice(1)}</strong></h2>
   <p>${data[0].phonetic===undefined ? "" : data[0].phonetic }</p>
   <p>${data[0].meanings[0].partOfSpeech}</p>
   <p><strong>Meaning: </strong>${definitions.definition}</p>
-  <p><strong>Example: </strong>"${definitions.example}"</p>
-  <p>Synonym: ${definitions.synonyms.length===0?"Unavailable":definitions.synonyms}</p>
+  <p><strong>Example: </strong>"${definitions.example?definitions.example:"Sorry, no example available"}"</p>
+  <p><strong>Synonym: </strong>${definitions.synonyms.length===0?"Unavailable":definitions.synonyms}</p>
   `;
   }catch(exception){
     result.innerHTML='<p>Sorry, your word could not be found</p>'
